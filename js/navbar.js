ea00b35ae301cd9fb8dbd2bbc9ad00ff4a859f77
BlueGum24 <bluegumdev@gmail.com>
2018-07-23 20:24:28 +0200

    landing-page-update

---
+++ js/navbar.js
@@ -0,0 +1,28 @@
+
+// Menu-toggle button
+
+$(document).ready(function() {
+  $(".menu-icon").on("click", function() {
+    $("nav").toggleClass("showing");
+    $("nav ul").toggleClass("showing");
+  });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+  if($(window).scrollTop()) {
+    $('nav').addClass('black');
+  }
+
+  else {
+    $('nav').removeClass('black');
+  }
+})
+
+$('.thumbnail').hover(function(){
+  $(this).children(".description").slideDown();
+},
+                      function(){
+  $(this).children(".description").fadeOut();
+}); 
